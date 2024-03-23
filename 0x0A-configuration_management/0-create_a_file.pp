f937e6aa18db4300e9b2476c892876b85e8800cb
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-23 03:06:33 +0100

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Define the file resource to create '/tmp/school'
+
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
