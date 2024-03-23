0c2a95749c2c75c5d52dd5096ad0379d3a76a1a7
garie-lhilarion <gabrielhilarion@gmail.com>
2024-03-23 03:34:59 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# Install Flask package with version 2.1.0
+# Define an exec resource to install Flask using pip3
 
-package { 'python3-flask':
-  ensure => '2.1.0',
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => ['PATH=/usr/bin'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
