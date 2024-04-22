97dd29a6d9a6c3daedd9538d7b0efb564b111e92
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-04-22 16:58:24 +0100

    Add a space after a tab

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -28,7 +28,7 @@ def get_employee_todo_progress(employee_id):
 
     for task in todo_data:
         if task['completed']:
-            print(f"\t{task['title']}")
+            print(f"\t {task['title']}")
 
 
 if __name__ == "__main__":
