d7626544f0c222e94d5cf0c90c60d523d3bbee76
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-05-08 03:48:57 +0200

    Complete refactotr code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and prints the titles of the 
-first 10 hot posts for a given subreddit.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the 
-    first 10 hot posts for a given subreddit.
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        None
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"} 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        for post in posts:
-            title = post["data"]["title"]
-            print(title)
-    else:
-        print("None")
 
 if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    top_ten(subreddit)
+    top_ten(argv[1])
